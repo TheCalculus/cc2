a5248292418803df41d68380a70c5579e70a167a
TheCalculus <seven.four.six.five@proton.me>
2024-08-29 21:12:58 +1000

    basic ast node types

--- include/parse.h
+++ include/parse.h
@@ -1,5 +1,27 @@
 #ifndef PARSE_H
 
+// https://godbolt.org/z/bsndGaf3h
 
+struct AstNode;
+
+typedef
+struct AstNode {
+    union {
+        struct VarDecl;
+
+        struct FunctionDecl;
+        struct ParamVarDecl;
+    } DeclStmt;
+
+    struct ImplicitCast;
+    struct ExplicitCast;
+
+    struct ReturnStmt;
+
+    struct CallExpr;
+
+    struct BinaryExp;
+    struct Unary;
+} AstNode;
 
 #endif
